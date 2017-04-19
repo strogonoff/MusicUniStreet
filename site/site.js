21379cbe003073b0dd25457a39cf720f67842231
Anton Strogonoff <anton@strogonoff.name>
2017-04-20 00:19:33 +0700

    WIP: playing with layout, implemented card switcher

--- site/site.js
+++ site/site.js
@@ -2,6 +2,8 @@ import { button, autoInit } from 'material-components-web';
 import '@material/button/mdc-button.scss';
 import '@material/fab/mdc-fab.scss';
 
+import _ from 'lodash';
+
 
 // Webpack: include these assets
 
@@ -15,3 +17,28 @@ import './style.css';
 // Initialize MDC
 
 autoInit()
+
+
+// Initialize card content switcher
+
+let initCardSwitcher = (...elements) => {
+    const STARTING_ZINDEX = 1000;
+    let cards = [];
+
+    function handleClick () {
+        this.style.zIndex = _.min(cards.map((el) => {
+            return el.style.zIndex;
+        })) - 1;
+    }
+
+    for (let el of elements) {
+        var zIndex = -1 * elements.indexOf(el) + STARTING_ZINDEX;
+        el.style.zIndex = zIndex;
+
+        el.addEventListener('click', handleClick);
+
+        cards.push(el);
+    }
+}
+
+initCardSwitcher(...document.getElementsByClassName('space'));
